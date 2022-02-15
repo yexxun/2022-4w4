ee874ba11c9bef8fe1fc3d1d1598dad4e373ce94
Eddy Martin <eddypmartin@gmail.com>
2022-02-15 13:11:51 -0500

    sem-4 c-1 wp_nav_menu, mixin

--- footer.php
+++ footer.php
@@ -3,9 +3,16 @@
     <p class="footer__presentation">Un site web fait par une élève du TIM à l'aide de Wordpress.</p>
     <h3 class="footer__author">Fait par Florence Lemieux Cayer</h3>
 
-<?php wp_nav_menu(array(
+<?php 
+
+$icone = '<svg width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#f00"><path fill-rule="evenodd" d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>';
+wp_nav_menu(array(
                     "menu"=>"simple",
-                    "container"=>"nav")); ?>
+                    "container"=>"nav",
+                    "container_class"=>"site__footer__menu",
+                    "menu_class"=>"site__footer__menu__ul",
+
+                    "link_before"=>$icone)); ?>
 </footer>
 </body>
 
--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php 
 
 function cidw_4w4_enqueue(){
-    wp_enqueue_style('style_css', get_stylesheet_uri());
+    //wp_enqueue_style('style_css', get_stylesheet_uri());
+    wp_enqueue_style('4w4-le-style', get_template_directory_uri() . '/style.css', array(), filemtime(get_template_directory() . '/style.css'), false);
 }
 
 add_action("wp_enqueue_scripts", "cidw_4w4_enqueue");
