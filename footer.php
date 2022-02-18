a3cb84624ac42bbe56e9f7de0cf757eb11e716ff
yeeun Kim <yeeun1214@hotmail.com>
2022-02-18 13:51:37 -0500

    entête style.scss lien github

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="site__footer">
-    <h2 class="footer__titre">Le beau footer</h2>
+    <h2 class="footer__titre"></h2>
     <p class="footer__presentation">Un site web fait par une élève du TIM à l'aide de Wordpress.</p>
-    <h3 class="footer__author">Fait par Florence Lemieux Cayer</h3>
+    <h3 class="footer__author">yeeun Kim</h3>
 
 <?php 
 
