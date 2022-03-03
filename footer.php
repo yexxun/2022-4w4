fcbc58137880d3cee758c7ceaf0f435016c7f2e0
Yeeun Kim <e1978196@cmaisonneuve.qc.ca>
2022-03-03 17:29:58 -0500

    modification de footer ul en column

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer class="site__footer">
+ <footer class="site__footer">
     <h2 class="footer__titre"></h2>
     <p class="footer__presentation">Un site web fait par une élève du TIM à l'aide de Wordpress.</p>
     <h3 class="footer__author">yeeun Kim</h3>
