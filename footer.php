3ba103aee4405852dc0dedeae72f24430e498b08
Yeeun Kim <e1978196@cmaisonneuve.qc.ca>
2022-04-26 12:55:45 -0400

    s12-c2 hover boîte

--- footer.php
+++ footer.php
@@ -40,6 +40,13 @@
 
  </div>
 </footer>
-</body>
+<div class="boite__modale">
+     <button class="boite__modale__fermeture">X</button>
+     <p class="boite__modale__texte">
+          Ceci est un premier test de boîte modale
+     </p>
+</div>
 <?php wp_footer(); ?>
+</body>
+
 </html>
