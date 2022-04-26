eac5ae0cdeaf726d1104fd717d578786b73e51f2
Yeeun Kim <e1978196@cmaisonneuve.qc.ca>
2022-04-26 13:01:19 -0400

    s12-c2 Boite modale étape #1 transition CSS

--- functions.php
+++ functions.php
@@ -5,7 +5,10 @@ function cidw_4w4_enqueue(){
     //wp_enqueue_style('style_css', get_stylesheet_uri());
     wp_enqueue_style('cidw-4w4-le-style', get_template_directory_uri() . '/style.css', array(), filemtime(get_template_directory() . '/style.css'), false);
     wp_enqueue_style('cidw-4w4-police-google',"https://fonts.googleapis.com/css2?family=Montserrat&display=swap", false);
-
+    wp_enqueue_script('cidw-4w4-boite-modale', 
+                        get_template_directory_uri() . '/javascript/boite-modale.js', 
+                        array(), filemtime(get_template_directory() . '/style.css'), 
+                        false);
 
 }
 
