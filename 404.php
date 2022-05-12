600b968e7e3211b65c961b3d07661ec61e6a6f62
Yeeun Kim <e1978196@cmaisonneuve.qc.ca>
2022-05-12 16:45:33 -0400

    s14-yk ajout d'un hover pour menu-accueil-container et menu-evenement-container

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header() ?>
+<main class="site__main">
+      <h1>---- 404.php ------</h1>
+     <section class="erreur-404">
+          <h1>La page n'existe pas</h1>
+     </section>
+   
+
+</main>
+<?php get_footer() ?>
